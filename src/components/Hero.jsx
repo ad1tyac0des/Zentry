5decb7b5c3b04668cda7ee05a6dc390bf8b2d59c
Aditya Singh <adityasingh740745@gmail.com>
2024-11-27 19:54:53 +0530

    feat(Hero): enhance video transition animation on click

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import { useState, useRef } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
@@ -23,6 +25,36 @@ const Hero = () => {
 
         setCurrentIndex(upcomingVideoIndex);
     };
+
+    // Handle Video Click Animation on Hero Page
+    useGSAP(
+        () => {
+            if (hasClicked){
+                gsap.set("#next-video", {visibility: "visible"});
+
+                gsap.to("#next-video", {
+                    transformOrigin: "center center",
+                    scale: 1,
+                    width: "100%",
+                    height: "100%",
+                    duration: 1,
+                    ease: "power1.inOut",
+                    onStart: () => nextVideoRef.current.play(),
+                })
+
+                gsap.from("#current-video", {
+                    transformOrigin: "center center",
+                    scale: 0,
+                    duration: 1.5,
+                    ease: "power1.inOut",
+                })
+            }
+        },
+        {
+            dependencies: [currentIndex],
+            revertOnUpdate: true,
+        }
+    );
 
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
@@ -61,7 +93,7 @@ const Hero = () => {
                     />
 
                     <video
-                        // autoPlay
+                        autoPlay
                         loop
                         muted
                         src={getVideoSrc(
