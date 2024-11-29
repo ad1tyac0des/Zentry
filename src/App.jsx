b7e2162aa2fe38e3a153104b1d416341b5567c94
Aditya Singh <adityasingh740745@gmail.com>
2024-11-29 21:17:52 +0530

    feat: add smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import About from "./components/About"
-import Hero from "./components/Hero"
+import { useEffect } from "react";
+import Lenis from "lenis";
+import "lenis/dist/lenis.css";
+
+import About from "./components/About";
+import Hero from "./components/Hero";
 
 const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      autoRaf: true,
+    });
+  }, []);
+
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
       <About />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
