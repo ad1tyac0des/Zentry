d6a27faf7b50670e2f7addeb343e77acc1bf98a6
Aditya Singh <adityasingh740745@gmail.com>
2024-12-09 00:08:04 +0530

    feat(Button): add interactive text hover effect

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,23 @@
-const Button = ({id, title, leftIcon, rightIcon, containerClass}) => {
+const Button = ({ id, title, leftIcon, rightIcon, containerClass }) => {
   return (
-    <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden outline-none rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
-        {leftIcon}
+    <button
+      id={id}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden outline-none rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}
+    >
+      {leftIcon}
 
-        <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
-            <div>
-                {title}
-            </div>
-        </span>
+      <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
+        <div className="translate-y-0 skew-y-0 transition duration-500 group-hover:translate-y-[-160%] group-hover:skew-y-12">
+          {title}
+        </div>
+        <div className="absolute translate-y-[164%] skew-y-12 transition duration-500 group-hover:translate-y-0 group-hover:skew-y-0">
+          {title}
+        </div>
+      </span>
 
-        {rightIcon}
+      {rightIcon}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
